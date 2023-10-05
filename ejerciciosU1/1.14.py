87a026a3176afb87bafcac5471d2f6e6b6439b4a
mbougar303 <mbougar303@g.educaand.es>
2023-10-05 18:26:51 +0200

    Prueba comit

---
+++ ejerciciosU1/1.14.py
@@ -0,0 +1,8 @@
+def main():
+    n = int(input("Introduzca un número entero: "))
+    m = int(input("Introduzca otro número entero: "))
+    print(f"La división de {n} entre {m} da un cociente {n // m} y un resto {n % m}")
+
+
+if __name__ == "__main__":
+    main()   
