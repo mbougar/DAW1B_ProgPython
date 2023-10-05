331221af1b7a373912368ff9bc0986a06d89c98f
mbougar303 <mbougar303@g.educaand.es>
2023-10-05 17:57:58 +0200

    save

--- ejerciciosU1/1.11.py
+++ ejerciciosU1/1.11.py
@@ -1,7 +1,7 @@
 def main():
     n = int(input("Dame un número entero: "))
     suma = 0
-    for i in range(1,n+1):   #la formula dada es: suma = int(n*(n+1)/2)sd
+    for i in range(1,n+1):   #la formula dada es: suma = int(n*(n+1)/2)
         suma += i
     print(f"La suma de todos los números enteros desde 1 hasta {n} es: {suma}")
 
